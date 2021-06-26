3b1a7ee45f3dcd95493245897d873647352bad0e
Abhinav Agnihotri <Aagnihotri7347@conestogac.on.ca>
2021-06-27 03:32:13 +0530

    TriangleSolver class

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -6,7 +6,7 @@ namespace Assignment2
     {
         public static void Main(string[] args)
         {
-           UserInput();
+            UserInput();
         }
 
         public static int UserInput()
@@ -20,8 +20,6 @@ namespace Assignment2
                     Console.WriteLine("1. Enter triangle dimensions");
                     Console.WriteLine("2. Exit\n");
                     Console.WriteLine("Please select a valid menu option i.e. either 1 or 2");
-
-
                     choice = Convert.ToInt32(Console.ReadLine());
 
                     if (choice == 1 || choice == 2)
@@ -29,32 +27,21 @@ namespace Assignment2
                         switch (choice)
                         {
                             case 1:
-
-                                int first_value, second_value, third_value;
                                 Console.WriteLine("Enter the first side (1st integer value) of a triangle");
-                                first_value = Convert.ToInt32(Console.ReadLine());
+                                int first_value = Convert.ToInt32(Console.ReadLine());
                                 Console.WriteLine("Enter the second side (2nd integer value) of a triangle");
-                                second_value = Convert.ToInt32(Console.ReadLine());
+                                int second_value = Convert.ToInt32(Console.ReadLine());
                                 Console.WriteLine("Enter the third side (3rd integer value) of a triangle");
-                                third_value = Convert.ToInt32(Console.ReadLine());
-                                if (first_value >= 0 && second_value >= 0 && third_value >= 0)
+                                int third_value = Convert.ToInt32(Console.ReadLine());
+                                if (first_value != 0 && second_value != 0 && third_value != 0)
                                 {
-                                    if (first_value != second_value && first_value != third_value)
-                                    {
-                                        Console.WriteLine("This is a Scalene Triangle because all 3 sides are unequal\n");
-                                    }
-                                    else if (first_value == second_value && first_value != third_value || first_value == third_value && second_value != third_value)
-                                    {
-                                        Console.WriteLine("This is an Isosceles Triangle because 2 sides are equal\n");
-                                    }
-                                    else if (first_value == second_value && second_value == third_value)
-                                    {
-                                        Console.WriteLine("This is an Equilateral Triangle because all 3 sides are equal\n");
-                                    }
+                                    string result = TriangleSolver.Analyze(first_value, second_value, third_value);
+                                    Console.WriteLine(result);
                                 }
                                 else
-                                    Console.WriteLine("Based on your input values, it doesn't form any triangle.\n");
-                                    break;
+                                    Console.WriteLine("On the basis of your input values, it doesn't form any triangle.");
+
+                                break;
 
                             case 2:
                                 Console.WriteLine("Good Bye! See you soon...");
--- Assignment2/TriangleSolver.cs
+++ Assignment2/TriangleSolver.cs
@@ -6,6 +6,34 @@ namespace Assignment2
 {
    public static class TriangleSolver
     {
-
+        
+        public static string Analyze(int a, int b, int c)
+        {
+            if (a >= 0 && b >= 0 && c >= 0)
+            {
+                if (a != b && a != c)
+                {
+                    string ScaleneTriangle = "This is a Scalene Triangle because all 3 sides are unequal\n";
+                    return ScaleneTriangle;
+                }
+                else if (a == b && a != c || a == c && b != c)
+                {
+                    string IsoscelesTriangle = "This is an Isosceles Triangle because 2 sides are equal\n";
+                    return IsoscelesTriangle;
+                }
+                else if (a == b && b == c)
+                {
+                    string EquilateralTriangle = "This is an Equilateral Triangle because all 3 sides are equal\n";
+                    return EquilateralTriangle;
+                }
+            }
+            else
+            {
+                string NoTriangle = "Based on your input values, it doesn't form any triangle.\n";
+                return NoTriangle;
+            }
+            return "No";
+           
+        }
     }
 }
